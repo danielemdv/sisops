3af7b2445eb20a3863bbe94a9b56109ddeb248eb
sisops <gangsterdan@gmail.com>
2017-03-17 06:58:37 -0700

    Fix original master code

--- thread/myMultiFactorial.c
+++ thread/myMultiFactorial.c
@@ -5,15 +5,15 @@
 int main(int argc, char *argv[]) { /* sequential code */
 
 
-int i, N=7,ans;
+int i, N=7,ans, numThreads = omp_get_max_threads();
 
-int ress[4]; //Array to store pre answers (before joining)...
+int ress[numThreads]; //Array to store pre answers (before joining)...
 //Initialize the array to 1s
-for(i = 1; i < 4; i++){
+for(i = 0; i < numThreads; i++){
   ress[i] = 1;
 }
 
-
+printf("DEBUG NUM THREADS %d", numThreads);
 
 
           /* sequential code */
@@ -21,15 +21,15 @@ for(i = 1; i < 4; i++){
 for(i=1;i<N;i++) {
   //Note the for starts at 1 cause we're going to ignore 0 in the multiplication.
 
-    ress[omp_get_thread_num()] = /*ress[omp_get_thread_num()]**/i*(i+1); //Calculates the products and stores them seperately
+    ress[omp_get_thread_num()] = ress[omp_get_thread_num()]*(i+1); //Calculates the products and stores them seperately
     printf("Calculando entrada %d\n",i);
     //sleep(1);
 }
 
 printf("Integrating answer");
 
-ans = ress[0];
-for(i = 1; i < 4; i++){
+ans = 1;
+for(i = 0; i < numThreads; i++){
   //note for starts at 1
   ans = ans * ress[i];
 }
